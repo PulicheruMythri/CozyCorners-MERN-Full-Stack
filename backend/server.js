2e227017c04ae5c67e3252864f57a8ee96d1a081
Pulicheru Mythri <pulicherumythri2930@gmail.com>
2025-12-11 12:11:59 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contact", contactRoutes);
+
+app.get("/", (req, res) => res.send("CozyCorners API Running"));
+
+app.listen(5000, () => console.log("Server started on port 5000"));
